c64cd41e0350150bba88bb0b874ff62b9e1921c7
LUJAIN <147299505+LujainYahia@users.noreply.github.com>
2024-05-31 18:54:57 +0300

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,4 @@
-//
-//  ContentView.swift
-//  Artector
-//
+
 //  Created by Lujain Yhia on 11/06/1445 AH.
 //
 
@@ -12,9 +9,9 @@ import CoreImage
 struct ContentView: View {
     @State private var error: Bool = false
     @State private var predictedImage: UIImage?
-    let labels = ["Blind Light", "Builders", "Composition","Dark sea","Fosa","Our generous land","Still life with fish ","The land of solidities","Thursday Market","Untitled (Desert series)", "Untitled 1987", "Untitled 1988"]
-    @State private var classificationLabel = ""
-    @State private var selectedPainting: Painting?
+    let labels = ["Data Laple"]
+    @State private var classification label = ""
+    @State private var selectedData: Data?
     @State private var navigateToResultPage = false
     
     var capturedImage: UIImage
@@ -41,21 +38,15 @@ struct ContentView: View {
             Button("Scan") {
                 classifyImage(capturedImage: capturedImage)
             }
-            .foregroundColor(Color.white)
+            
             .fontWeight(.medium)
             .font(.title3)
             .frame(width: 342,height: 50)
             .background(Color("PrimaryColor"))
-            .cornerRadius(10)
 
-            NavigationLink(destination: ResultPage(painting: selectedPainting, image: predictedImage), isActive: $navigateToResultPage) {
-//                Text("Show Painting Info")
-//                    .padding()
-//                    .frame(width: 250)
-//                    .foregroundColor(Color.black)
-//                    .background(Color.white)
-//                    .cornerRadius(10)
-//                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 2))
+
+            NavigationLink(destination: ResultPage(DATA: selectedData, image: predictedImage), isActive: $navigateToResultPage) {
+
             }
             .padding()
 
@@ -80,21 +71,19 @@ struct ContentView: View {
             self.classificationLabel = "Match: \(Int(confidence * 100))%"
 
             if confidence >= 0.85 {
-                if let matchedPainting = paintings[prediction.target] {
-                    self.selectedPainting = matchedPainting
+                if let matchedData = Data[prediction.target] {
+                    self.selectedData = matchedData
                     self.predictedImage = capturedImage
                     self.navigateToResultPage = true
                 } else {
-                    // If there's no matched painting found within the high confidence level
-                    self.selectedPainting = nil
+                    
+                    self.selectedData = nil
                     self.predictedImage = nil
-                    // You might want to handle this case differently or leave it as is
                 }
             } else {
-                // For confidence less than 85%, show the captured image with a message
-                self.selectedPainting = nil
+                self.selectedData = nil
                 self.predictedImage = capturedImage // Show the captured image
-                self.classificationLabel = "There is no painting, try again"
+                self.classificationLabel = "There is no match, try again"
             }
 
             // Catch block remains unchanged
